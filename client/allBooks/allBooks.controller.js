d274d93aaafd64d2402954b5011525a00bfbccc3
andela-oagboola <olaide.agboola@andela.co>
2016-10-13 13:17:42 +0100

    field addition for bookcover

--- client/allBooks/allBooks.controller.js
+++ client/allBooks/allBooks.controller.js
@@ -6,9 +6,6 @@
   function booksListController(booksService){
     var books = this;
 
-    //placeholder for summary
-    books.summary = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum finibus et tortor eu imperdiet. Nam imperdiet elit ut augue ultricies, quis sagittis arcu semper. Vestibulum eu lectus vitae est faucibus porttitor. Pellentesque eget nunc lectus. Donec tempus hendrerit urna, et condimentum mauris.';
-
     booksService.getBooks().then(function(response){
       books.bookCollection = response.data
     });
