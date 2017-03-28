74211efdf6e2d1daa1a7651f04d1fc45a4672580
Mayowa Falade <mayowa.falade@ithaka.org>
2017-03-29 00:19:39 +0100

    Add npm script to run app even when user doesn't have gulp installed globally.. refactor server

--- public/build.js
+++ public/build.js
@@ -142,46 +142,6 @@ bookedUpApp.run(['$rootScope', 'authenticationService','$location', function($ro
   }
 })();
 (function(){
-  angular.module('bookedUpApp')
-          .controller('HomeController', HomeController);
-
-  HomeController.$inject = ['authenticationService', '$location'];
-
-  function HomeController(authenticationService, $location){
-
-    //enable materialisecss tabs
-    $('ul.tabs').tabs();
-
-    var home = this;
-    home.userSignup = {};
-    home.userLogin = {};
-
-    home.signup = function(){
-      authenticationService.signup(home.userSignup).then(function(user){
-        if(user){
-          $location.path('/books');
-        }
-      }).catch(function(err){
-        if(err.data.errors.email.kind == 'user defined'){
-          alert('email already exists');
-        }
-        else {
-          alert('Error with signup');
-        }
-      })
-    }
-
-    home.login = function(){
-      authenticationService.login(home.userLogin).then(function(user){
-        $location.path('/books');
-      }).catch(function(err){
-        alert('Email or password incorrect');
-      });
-    }
-  }
-
-})();
-(function(){
   angular.module('bookedUpApp').controller('EditBookController', EditBookController);
 
   EditBookController.$inject = ['booksService','$routeParams', 'Upload', '$location']
@@ -223,6 +183,46 @@ bookedUpApp.run(['$rootScope', 'authenticationService','$location', function($ro
       }
     }
   }
+})();
+(function(){
+  angular.module('bookedUpApp')
+          .controller('HomeController', HomeController);
+
+  HomeController.$inject = ['authenticationService', '$location'];
+
+  function HomeController(authenticationService, $location){
+
+    //enable materialisecss tabs
+    $('ul.tabs').tabs();
+
+    var home = this;
+    home.userSignup = {};
+    home.userLogin = {};
+
+    home.signup = function(){
+      authenticationService.signup(home.userSignup).then(function(user){
+        if(user){
+          $location.path('/books');
+        }
+      }).catch(function(err){
+        if(err.data.errors.email.kind == 'user defined'){
+          alert('email already exists');
+        }
+        else {
+          alert('Error with signup');
+        }
+      })
+    }
+
+    home.login = function(){
+      authenticationService.login(home.userLogin).then(function(user){
+        $location.path('/books');
+      }).catch(function(err){
+        alert('Email or password incorrect');
+      });
+    }
+  }
+
 })();
 
 (function(){
